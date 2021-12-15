7cffe23ec926c6f03e3ab1c0255dd2c57c5dd878
Awesome Guy <ta.okundaye@gmail.com>
2021-12-15 15:30:05 +0000

    Created a greeting function

---
+++ hello-world.py
@@ -0,0 +1,36 @@
+from textfile import my_sentence
+
+print("Hello, World")
+'25635353dffvdcgdfd'
+_days_quote= "Nosa is a nice guy"
+
+# myarr= []
+# execfile( "textfile.py", myarr )
+# sent= exec(with open('textfile.py').read())
+"Nosa is a nice guy"
+"Nosa is a nice guy"
+"Nosa is a nice guy"
+"Nosa is a nice guy"
+"Nosa is a nice guy"
+"Nosa is a nice guy"
+
+# print(_days_quote)
+# print(my_sentence)
+
+# print(my_sentence_length)
+print(type(my_sentence))
+
+
+# Boolean
+print(type(True))
+False
+
+#Integer
+print(type(500_000_000))
+
+#Float
+print(type(100.5))
+
+
+
+
